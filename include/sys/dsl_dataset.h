06a25f50982a696ae1c56c1f4ea94fbf73615fc9
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2016-05-28 23:52:02 +0200

    Deactivate pool feature when setting a different compression
    Rough draft.

--- include/sys/dsl_dataset.h
+++ include/sys/dsl_dataset.h
@@ -223,6 +223,12 @@ typedef struct dsl_dataset {
 	 */
 	uint8_t ds_feature_activation_needed[SPA_FEATURES];
 
+	/*
+	 * Set if we need to deactivate the feature on this dataset this txg
+	 * (used only in syncing context).
+	 */
+	uint8_t ds_feature_deactivation_needed[SPA_FEATURES];
+
 	/* Protected by ds_lock; keep at end of struct for better locality */
 	char ds_snapname[ZFS_MAX_DATASET_NAME_LEN];
 } dsl_dataset_t;
